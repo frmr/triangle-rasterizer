986c881d5cbc0f906299de9cb88c908335b6eef5
Tom Farmer <farmergeddon@gmail.com>
2019-01-29 19:40:31 +0000

    Bilinear filtering (always repeat)

--- src/tr/trBuffer.hpp
+++ src/tr/trBuffer.hpp
@@ -16,7 +16,8 @@ namespace tr
 			m_width(width),
 			m_height(height),
 			m_floatWidth(float(width)),
-			m_floatHeight(float(height))
+			m_floatHeight(float(height)),
+			m_maxSize(std::numeric_limits<size_t>::max())
 		{
 			m_data.resize(width * height, T());
 		}
@@ -46,28 +47,76 @@ namespace tr
 			return m_data[y * m_width + x];
 		}
 
-		float fastFloor(const float x) const
-		{
-			const int xi = int(x);
-			return float(x < xi ? xi - 1 : xi);
-		}
-
-		T getAt(float x, float y, const bool filter, const TextureWrappingMode textureWrappingMode) const
+		T getAt(float u, float v, const bool filter, const TextureWrappingMode textureWrappingMode) const
 		{
 			if (textureWrappingMode == TextureWrappingMode::Clamp)
 			{
 				constexpr float upperLimit = 1.0f - std::numeric_limits<float>::epsilon();
 
-				x = std::clamp(x, 0.0f, upperLimit);
-				y = std::clamp(y, 0.0f, upperLimit);
+				u = std::clamp(u, 0.0f, upperLimit);
+				v = std::clamp(v, 0.0f, upperLimit);
 			}
 			else
 			{
-				x -= fastFloor(x);
-				y -= fastFloor(y);
+				u -= fastFloor(u);
+				v -= fastFloor(v);
 			}
 
-			return getAt(size_t(x * m_floatWidth), size_t(y * m_floatHeight));
+			u *= m_floatWidth;
+			v *= m_floatHeight;
+
+			if (filter)
+			{
+				u -= 0.5f;
+				v -= 0.5f;
+
+				const float  uFloor    = std::floor(u);
+				const float  vFloor    = std::floor(v);
+
+				size_t       x0        = size_t(uFloor);
+				size_t       y0        = size_t(vFloor);
+
+				const float  uDiff     = u - uFloor;
+				const float  vDiff     = v - vFloor;
+
+				const float  uOpposite = 1.0f - uDiff;
+				const float  vOpposite = 1.0f - vDiff;
+
+				size_t       x1        = x0 + 1;
+				size_t       y1        = y0 + 1;
+
+				if (x0 == m_maxSize)
+				{
+					x0 = m_width - 1;
+				}
+				else if (x1 == m_width)
+				{
+					x1 = 0;
+				}
+
+				if (y0 == m_maxSize)
+				{
+					y0 = m_height - 1;
+				}
+				else if (y1 == m_height)
+				{
+					y1 = 0;
+				}
+
+				const Vector4 topLeft     = (m_data.data() + (y0 * m_width + x0))->ToVector();
+				const Vector4 topRight    = (m_data.data() + (y0 * m_width + x1))->ToVector();
+				const Vector4 bottomLeft  = (m_data.data() + (y1 * m_width + x0))->ToVector();
+				const Vector4 bottomRight = (m_data.data() + (y1 * m_width + x1))->ToVector();
+
+				return (topLeft    * uOpposite + topRight    * uDiff) * vOpposite +
+				       (bottomLeft * uOpposite + bottomRight * uDiff) * vDiff;
+			}
+			else
+			{
+				return getAt(size_t(u), size_t(v));
+			}
+
+			
 		}
 
 		T* getData()
@@ -86,10 +135,18 @@ namespace tr
 		}
 
 	private:
+		float fastFloor(const float x) const
+		{
+			const int xi = int(x);
+			return float(x < xi ? xi - 1 : xi);
+		}
+
+	private:
 		const size_t   m_width;
 		const size_t   m_height;
 		const float    m_floatWidth;
 		const float    m_floatHeight;
+		const size_t   m_maxSize;
 		std::vector<T> m_data;
 	};
 }
--- src/tr/trColor.hpp
+++ src/tr/trColor.hpp
@@ -1,11 +1,43 @@
 #pragma once
 
+#include "../matrix/Vectors.h"
+
 namespace tr
 {
 #pragma pack(push,1)
 
 	struct Color
 	{
+		Vector4 ToVector() const
+		{
+			return Vector4(
+				float(blue),
+				float(green),
+				float(red),
+				float(alpha)
+			);
+		}
+
+		Color()
+		{
+		}
+
+		Color(const unsigned char blue, const unsigned char green, const unsigned char red, const unsigned char alpha) :
+			blue(blue),
+			green(green),
+			red(red),
+			alpha(alpha)
+		{
+		}
+
+		Color(const Vector4& vector) :
+			blue((unsigned char)vector[0]),
+			green((unsigned char)vector[1]),
+			red((unsigned char)vector[2]),
+			alpha((unsigned char)vector[3])
+		{
+		}
+
 		unsigned char blue;
 		unsigned char green;
 		unsigned char red;
