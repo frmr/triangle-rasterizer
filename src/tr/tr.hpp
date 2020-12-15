876a435ac1d7245f496d23bc082e0db6ed894e49
Tom Farmer <farmergeddon@gmail.com>
2020-12-15 22:56:57 +0000

    Removed default shaders

--- src/tr/tr.hpp
+++ src/tr/tr.hpp
@@ -1,12 +1,3 @@
 #pragma once
 
-#include <algorithm>
-#include <cassert>
-#include <cmath>
-#include <cstdlib>
-#include <limits>
-#include <vector>
-
 #include "trRasterizer.hpp"
-#include "trDefaultShader.hpp"
-#include "trDepthShader.hpp"
